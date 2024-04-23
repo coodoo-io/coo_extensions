41be281d9be097ba29c8bad8964240751cae5b71
Marcel Ploch <140072620+marcel-ploch-coodoo@users.noreply.github.com>
2024-04-23 15:34:56 +0200

    adding date is before, after or inBetween (#5)

--- lib/extensions/date_time.extension.dart
+++ lib/extensions/date_time.extension.dart
@@ -9,13 +9,30 @@ extension DateTimeExtension on DateTime {
   /// Indicates whether the DateTime object represents yesterday's date.
   bool get isYesterday {
     final yesterday = DateTime.now().subtract(const Duration(days: 1));
-    return yesterday.day == day &&
-        yesterday.month == month &&
-        yesterday.year == year;
+    return yesterday.day == day && yesterday.month == month && yesterday.year == year;
   }
 
   /// Indicates whether the DateTime object belongs to the same year as the current date and time.
   bool get isSameYear {
     return DateTime.now().year == year;
+  }
+
+  bool isAfterOrEqual(DateTime dateTime) {
+    final isAtSameMomentAs = dateTime.isAtSameMomentAs(this);
+    return isAtSameMomentAs | isAfter(dateTime);
+  }
+
+  bool isBeforeOrEqual(DateTime dateTime) {
+    final isAtSameMomentAs = dateTime.isAtSameMomentAs(this);
+    return isAtSameMomentAs | isBefore(dateTime);
+  }
+
+  bool isBetween(
+    DateTime fromDateTime,
+    DateTime toDateTime,
+  ) {
+    final isAfter = isAfterOrEqual(fromDateTime);
+    final isBefore = isBeforeOrEqual(toDateTime);
+    return isAfter && isBefore;
   }
 }
--- test/date.extension_test.dart
+++ test/date.extension_test.dart
@@ -25,8 +25,7 @@ void main() {
       expect(result, isTrue);
     });
 
-    test('isSameYear should return true for the same year as the current date',
-        () {
+    test('isSameYear should return true for the same year as the current date', () {
       // Arrange
       DateTime now = DateTime.now();
 
@@ -37,9 +36,7 @@ void main() {
       expect(result, isTrue);
     });
 
-    test(
-        'isSameYear should return false for a different year than the current date',
-        () {
+    test('isSameYear should return false for a different year than the current date', () {
       // Arrange
       DateTime otherYear = DateTime.now().subtract(const Duration(days: 365));
 
@@ -48,6 +45,74 @@ void main() {
 
       // Assert
       expect(result, isFalse);
+    });
+
+    group('isBeforeOrEqualTo or isAfterOrEqualTo', () {
+      test('if date is not after CurrentDate', () {
+        final curDate = DateTime(2024, 1, 1);
+        final testDate = DateTime(2024, 1, 2);
+
+        final result = curDate.isAfterOrEqual(testDate);
+        expect(result, false);
+      });
+
+      test('if date is after CurrentDate', () {
+        final curDate = DateTime(2024, 1, 3);
+        final testDate = DateTime(2024, 1, 2);
+
+        final result = curDate.isAfterOrEqual(testDate);
+        expect(result, true);
+      });
+
+      test('if date is equals CurrentDate', () {
+        final curDate = DateTime(2024, 1, 2);
+        final testDate = DateTime(2024, 1, 2);
+
+        final result = curDate.isAfterOrEqual(testDate);
+        expect(result, true);
+      });
+
+      test('if date is not before CurrentDate', () {
+        final curDate = DateTime(2024, 1, 1);
+        final testDate = DateTime(2024, 1, 2);
+
+        final result = curDate.isBeforeOrEqual(testDate);
+        expect(result, true);
+      });
+
+      test('if date is before CurrentDate', () {
+        final curDate = DateTime(2024, 1, 3);
+        final testDate = DateTime(2024, 1, 2);
+
+        final result = curDate.isBeforeOrEqual(testDate);
+        expect(result, false);
+      });
+
+      test('if date is equals CurrentDate', () {
+        final curDate = DateTime(2024, 1, 2);
+        final testDate = DateTime(2024, 1, 2);
+
+        final result = curDate.isBeforeOrEqual(testDate);
+        expect(result, true);
+      });
+    });
+
+    group('isBetween', () {
+      test('current date is not in between two dates', () {
+        final curDate = DateTime(2024, 1, 1);
+        final fromDate = DateTime(2024, 1, 2);
+        final untilDate = DateTime(2024, 1, 4);
+
+        expect(curDate.isBetween(fromDate, untilDate), false);
+      });
+
+      test('current date is in between two dates', () {
+        final curDate = DateTime(2024, 1, 2);
+        final fromDate = DateTime(2024, 1, 2);
+        final untilDate = DateTime(2024, 1, 4);
+
+        expect(curDate.isBetween(fromDate, untilDate), true);
+      });
     });
   });
 }
