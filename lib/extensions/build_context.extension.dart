1cc9e81582527709f3d2dd27f79f182fea89d414
Simon Stevens <simon.stevens@coodoo.de>
2024-10-21 17:12:40 +0200

    improve theme

--- lib/extensions/build_context.extension.dart
+++ lib/extensions/build_context.extension.dart
@@ -1,21 +1,20 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 /// Extension on the BuildContext class providing additional functionality related to UI building.
 extension BuildContextExtension on BuildContext {
   /// Indicates whether the app is currently in light mode.
   bool get isLightMode =>
-      WidgetsBinding.instance.platformDispatcher.platformBrightness ==
+      Theme.of(this).brightness ==
       Brightness.light;
 
   /// Indicates whether the app is currently in dark mode.
   bool get isDarkMode =>
-      WidgetsBinding.instance.platformDispatcher.platformBrightness ==
+      Theme.of(this).brightness ==
       Brightness.dark;
 
   /// Retrieves the width of the device screen.
-  double get deviceWidth => MediaQuery.of(this).size.width;
+  double get deviceWidth => MediaQuery.sizeOf(this).width;
 
   /// Retrieves the height of the device screen.
-  double get deviceHeight => MediaQuery.of(this).size.height;
+  double get deviceHeight => MediaQuery.sizeOf(this).height;
 }
